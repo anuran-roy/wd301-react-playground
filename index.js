4e9d0fe5b2f4a427b3f923d19d014f6e3ca17713
anuran <anuranroy02@gmail.com>
2022-03-08 23:34:27 +0530

    INIT

---
+++ index.js
@@ -0,0 +1,13 @@
+var text = "Hello world!";
+console.log(text);
+var john = [
+    {
+        name: "John",
+        age: 24
+    },
+    {
+        name: "Jane",
+        age: 22
+    }
+];
+console.log(john);
---
+++ index.ts
@@ -0,0 +1,20 @@
+let text: string = "Hello world!";
+console.log(text);
+
+interface Person {
+    name: string,
+    age: number
+}
+
+let john : Person[] = [
+    {
+        name: "John",
+        age:24
+    },
+    {
+        name: "Jane",
+        age: 22
+    }
+]
+
+console.log(john);
